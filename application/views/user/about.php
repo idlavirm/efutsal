de271a4ddb9eab54703319568bb9d708389db3fc
idlavirm <mrivv10@gmail.com>
2022-06-05 12:57:24 +0700

    web programming 3

---
+++ application/views/user/about.php
@@ -0,0 +1,23 @@
+<!-- About -->
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<section id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading text-uppercase">Apa Itu E-Futsal Field</h2>
+        <h3 class="section-subheading text-muted">E-Futsal Field adalah sebuah aplikasi web penyewaan lapangan futsal. Anda dapat menyewa lapangan futsal yang ada di situs ini lalu cari lapangan yang anda ingin "Booking". Anda juga dapat melakukan cek jadwal dahulu sebelum memesan dan atur jadwal yang anda inginkan.
+          <br><br><i class="fas fa-phone-square"> Contact us : 08123456789</i><br><br><i class="fas fa-envelope"> Email : kelompok4@gmail.com</i>
+        </h3>
+      </div>
+
+      <body>
+        <h2 class="section-heading text-uppercase">Temukan Kami</h2>
+        <?php echo $map['js']; ?>
+
+        <body>
+          <?php echo $map['html']; ?>
+        </body>
+    </div>
+  </div>
+  </div>
+</section>
